60c47dee77d5f4ee74182e55a6c144d62239299f
klimvov <klimashevskijvova@gmail.com>
2022-11-24 17:09:12 +0300

    user

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,7 @@
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
     <body class="font-sans antialiased">
         @inertia
